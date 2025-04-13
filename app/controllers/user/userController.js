e6367697e6d63bd6f41230193010b01d00492b9b
Valery1029 <julisco2410@gmail.com>
2025-04-12 21:56:51 -0500

    Se hacen cambios en los controladores del login y user, además de cambios en el main, login_view y se agrega el archivo constant

--- app/controllers/user/userController.js
+++ app/controllers/user/userController.js
@@ -1,27 +1,44 @@
-//Constantes
+/* Author:DIEGO CASALLAS
+* Date:20/03/2025
+* Descriptions:This is controller User 
+* **/
+
+/* The provided code snippet is declaring and initializing several constants and variables in a
+JavaScript file. Here is a breakdown of what each of them is doing: */
 const formId = 'my-form';
 const modalId = 'my-modal';
 const model = 'users';
 const tableId = '#table-index';
 const preloadId = 'preloadId';
 const classEdit = 'edit-input';
-const textConfirm = 'Press a button!\nEither OK or Cancel';
+const textConfirm = 'Press a button!\nEither OK or Cancel.';
 const btnSubmit = document.getElementById('btnSubmit');
 const mainApp = new Main(modalId, formId, classEdit, preloadId);
-
-//Variables
+const roleSelectId="role";
+const myId="User_id";
+/* These lines of code are declaring and initializing variables in a JavaScript file. Here is a
+breakdown of what each variable is used for: */
 var insertUpdate = true;
 var url = "";
 var method = "";
 var data = "";
 var resultFetch = null;
 
-//Window
+
+/* The `window.addEventListener('load', (event) => { show(); });` code snippet is adding an event
+listener to the `load` event of the `window` object. When the window has finished loading (i.e.,
+when all resources on the page have been loaded), the `show()` function is called. This ensures that
+the `show()` function is executed once the entire page has loaded, allowing any necessary
+initialization or data retrieval operations to be performed at that time. */
 window.addEventListener('load', (event) => {
   show();
 });
 
-//Function 'showStatus'
+/**
+ * The function `showStatus` disables all form elements, resets the form, enables a button, and
+ * retrieves the status based on the provided ID.
+ * @param id - The `id` parameter is used to identify a specific status that needs to be displayed.
+ */
 function showId(id) {
   mainApp.disabledFormAll();
   mainApp.resetForm();
@@ -29,25 +46,32 @@ function showId(id) {
   getDataId(id);
 }
 
-//Function 'show'
-function show() {
-  mainApp.disabledFormAll();
-  mainApp.resetForm();
-  btnEnabled(true);
-  getUsers();
-}
 
-//Function 'newStatus'
+/**
+ * The function "show" calls the "getUsers" and "getRoles" functions.
+ */
+function show() {
+  getUsers();
+  getRoles();
+}
+/**
+ * The function `newStatus` enables a form, resets it, sets a flag, disables a button, and shows a
+ * modal.
+ */
 function add() {
   mainApp.enableFormAll();
   mainApp.resetForm();
-  mainApp.hiddenMessageError();
   insertUpdate = true;
   btnEnabled(false);
   mainApp.showModal();
 }
 
-//Function 'editStatus'
+/**
+ * The function `editStatus` disables form editing, resets the form, sets `insertUpdate` to false,
+ * disables a button, and retrieves the status ID.
+ * @param id - The `id` parameter in the `editStatus` function is used to identify the specific status
+ * that needs to be edited.
+ */
 function edit(id) {
   mainApp.disabledFormEdit();
   mainApp.resetForm();
@@ -56,9 +80,15 @@ function edit(id) {
   getDataId(id);
 }
 
-//Function 'deleteStatus'
+/**
+ * The function `deleteStatus` is an asynchronous function that sends a GET request to delete a status
+ * based on the provided ID, with a confirmation prompt and subsequent data retrieval and page reload.
+ * @param id - The `id` parameter in the `deleteStatus` function represents the unique identifier of
+ * the status that you want to delete. This identifier is used to specify which status entry should be
+ * deleted from the system.
+ */
 async function delete_(id) {
-  if (confirm(textConfirm)== true) {
+  if (confirm(textConfirm) == true) {
     method = 'DELETE';
     url = URL + URI_USER + id;
     data = "";
@@ -66,13 +96,13 @@ async function delete_(id) {
     resultFetch.then(response => response.json())
       .then(data => {
         console.log(data);
-        //Set data form
+        ///Set data form 
         reloadPage();
         mainApp.hiddenPreload();
       })
       .catch(error => {
         console.error(error);
-        //Hidden Preload
+        //hidden Preload 
         mainApp.hiddenPreload();
       })
       .finally();
@@ -80,7 +110,12 @@ async function delete_(id) {
   }
 }
 
-//Function 'get by id'
+/**
+ * The function `getDataId` makes an asynchronous GET request to retrieve data based on an ID, then
+ * processes the response data to update the form, show a modal, and handle errors.
+ * @param id - The `id` parameter in the `getDataId` function is used to specify the ID of the status
+ * you want to retrieve.
+ */
 async function getDataId(id) {
   method = 'GET';
   url = URL + URI_USER + id;
@@ -88,65 +123,116 @@ async function getDataId(id) {
   resultFetch = getData(data, method, url);
   resultFetch.then(response => response.json())
     .then(data => {
-      //Set data form
+      // console.log(data);
+      ///Set data form 
       mainApp.setDataFormJson(data);
-      //Show Modal
+      //show Modal 
       mainApp.showModal();
-      //Hidden Preload
+      //hidden Preload 
       mainApp.hiddenPreload();
     })
     .catch(error => {
       console.error(error);
-      //Hidden Preload
+      //hidden Preload 
       mainApp.hiddenPreload();
     })
     .finally();
 }
 
-//Function getUsers
+/**
+ * The function `getUsers` makes an asynchronous request to fetch user data, creates a table with the
+ * data, and then refreshes the table while handling errors and hiding a preload element.
+ */
 async function getUsers() {
   method = 'GET';
   url = URL + URI_USER;
   data = mainApp.getDataFormJson();
-  
   resultFetch = getData(data, method, url);
   resultFetch.then(response => response.json())
     .then(data => {
-      //Create table
-      mainApp.createTable(data, 'tbody', true);
+      //console.log(data);
+      ///create table
+      mainApp.createTable(data, "tbody", true);
       refreshTable();
-      //Hidden Preload
+      //hidden Preload 
       mainApp.hiddenPreload();
     })
-    .catch(error => {
-      //Hidden Preload
+    .catch(err => {
+      //console.error(err);
+      //hidden Preload 
       mainApp.hiddenPreload();
     })
     .finally();
 }
 
-//Function 'Refresh Table'
+/**
+ * The function `getRoles` makes a GET request to a specified URL to fetch roles data, creates a select
+ * element based on the data, and then hides a preload element.
+ */
+async function getRoles() {
+  method = 'GET';
+  url = URL + URI_ROLE;
+  data = mainApp.getDataFormJson();
+  resultFetch = getData(data, method, url);
+  resultFetch.then(response => response.json())
+    .then(data => {
+      //console.log(data);
+      ///create select
+      mainApp.createSelect(data,roleSelectId);
+      //hidden Preload 
+      mainApp.hiddenPreload();
+    })
+    .catch(err => {
+      //console.error(err);
+      //hidden Preload 
+      mainApp.hiddenPreload();
+    })
+    .finally();
+}
+
+/**
+ * The function refreshTable() initializes a DataTable for a specified table element using jQuery.
+ */
 function refreshTable() {
   $(tableId).DataTable();
 }
-
-//Function 'Btn Enbled'
+/**
+ * The function `btnEnabled` is used to enable or disable a button based on the `type` parameter.
+ * @param type - The `type` parameter in the `btnEnabled` function is used to determine whether the
+ * button should be enabled or disabled. If `type` is `true`, the button will be disabled, and if
+ * `type` is `false`, the button will be enabled.
+ */
 function btnEnabled(type) {
   btnSubmit.disabled = type;
 }
 
-//Function getData
+/**
+ * The function `getData` is an asynchronous function that makes a fetch request with specified data,
+ * method, and URL while showing a preload indicator.
+ * @param data - The `data` parameter in the `getData` function represents the data that you want to
+ * send in the request. It could be an object containing the information you want to send to the
+ * server, such as user input or any other relevant data needed for the request.
+ * @param method - The `method` parameter in the `getData` function specifies the HTTP request method
+ * to be used for fetching data. It can be either "GET" or another HTTP method like "POST", "PUT",
+ * "DELETE", etc., depending on the type of request you want to make to the specified URL
+ * @param url - The `url` parameter in the `getData` function is the URL to which the HTTP request will
+ * be sent. It specifies the location of the resource that the function will interact with.
+ * @returns The function `getData` is returning the result of the `fetch` function called with the
+ * provided `url` and `parameters`. The `fetch` function performs an asynchronous request to the
+ * specified `url` using the given `parameters` and returns a Promise that resolves to the Response to
+ * that request.
+ */
 async function getData(data, method, url) {
   var parameters;
-  //Show Preload
+  //Show Preload 
   mainApp.showPreload();
-  if (method == 'GET') {
+  if (method == "GET") {
     parameters = {
       method: method,
       mode: 'cors',
       headers: {
         "Content-Type": "application/json",
-        "X-Requested-With": "XMLHttpResquest"
+        "X-Requested-With": "XMLHttpRequest"
       }
     }
   } else {
@@ -155,7 +241,7 @@ async function getData(data, method, url) {
       mode: 'cors',
       headers: {
         "Content-Type": "application/json",
-        "X-Requested-With": "XMLHttpResquest"
+        "X-Requested-With": "XMLHttpRequest"
       }
     }
     if (data != "") {
@@ -165,59 +251,68 @@ async function getData(data, method, url) {
   return await fetch(url, parameters);
 }
 
-//Method getForm
+
+
+/* The code snippet you provided is an event listener attached to the form element within the `mainApp`
+object. It listens for the `submit` event on the form and executes a series of actions when the form
+is submitted. Here is a breakdown of what the code is doing: */
 mainApp.getForm().addEventListener('submit', async function (event) {
   event.preventDefault();
   if (mainApp.setValidateForm()) {
-    //Show Preload
+    //Show Preload 
     mainApp.showPreload();
     if (insertUpdate) {
       method = 'POST';
       url = URL + URI_USER;
       data = mainApp.getDataFormJson();
+      //console.log(data);
       resultFetch = getData(data, method, url);
       resultFetch.then(response => response.json())
         .then(data => {
-          //Show Modal
+          console.log(data);
+          //show Modal 
           mainApp.hiddenModal();
           //Reload View
           reloadPage();
         })
         .catch(error => {
           console.error(error);
-          //Hidden Preload
+          //hidden Preload 
           mainApp.hiddenPreload();
         })
         .finally();
     } else {
       method = 'PUT';
       data = mainApp.getDataFormJson();
-      url = URL + URI_USER + data.id;
+      url = URL + URI_USER + data[myId];
       resultFetch = getData(data, method, url);
       resultFetch.then(response => response.json())
         .then(data => {
-          //Show Modal
+          //console.log(data);
+          //show Modal 
           mainApp.hiddenModal();
           //Reload View
           reloadPage();
         })
         .catch(error => {
           console.error(error);
-          //Hidden Preload
+          //hidden Preload 
           mainApp.hiddenPreload();
         })
         .finally();
     }
   } else {
-    alert('Data Validate');
+    alert("Data Validate");
     mainApp.resetForm();
   }
 });
-
-//Function 'reloadPage'
+/**
+ * The function `reloadPage` hides a preload element, waits for 500 milliseconds, and then reloads the
+ * page.
+ */
 function reloadPage() {
-  setTimeout(function() {
-    //Hidden Preload
+  setTimeout(function () {
+    //hidden Preload 
     mainApp.hiddenPreload();
     location.reload();
   }, 500);
--- app/system/main.js
+++ app/system/main.js
@@ -21,53 +21,55 @@ class Main {
     } else {
       arrayForm.push(document.getElementById(formId));
     }
-
     this.myForm = arrayForm;
+
     this.classEdit = classEdit;
     this.elementJson = {};
     this.fromData = new FormData();
     this.preload = document.getElementById(preloadId);
   }
 
+  /**
+   * The function `showPreload()` sets the display property of an element with the class `preload` to
+   * "block".
+   */
   showPreload() {
     this.preload.style.display = "block";
   }
 
-
+  /**
+   * The `hiddenPreload` function hides the preload element by setting its display style to "none".
+   */
   hiddenPreload() {
-    const objPreload = this.preload;
-    var op = 1;  // initial opacity
-    let fadeEffect = setInterval(function () {
-      if (op <= 0.1) {
-        objPreload.style.display = "none";
-        clearInterval(fadeEffect);
-      }
-      objPreload.style.opacity = op;
-      op -= 0.1;
-    }, 100, objPreload);
+    this.preload.style.display = "none";
   }
 
-
-  setLocationPage(url) {
-    setTimeout(() => {
-      window.location.href = url;
-    }, 1000);
-  }
-
+  /**
+   * The function showModal() displays a modal.
+   */
   showModal(position = 0) {
     this.myModal[position].show();
   }
 
+  /**
+   * The `hiddenModal` function hides a modal element.
+   */
   hiddenModal(position = 0) {
     this.myModal[position].hide();
   }
 
+  /**
+   * The getForm function returns the myForm property.
+   * @returns The `myForm` property is being returned from the `getForm` method.
+   */
   getForm(position = 0) {
     return this.myForm[position];
   }
 
+  /**
+   * The function `disabledFormAll` disables all input and select elements within a specified form.
+   */
   disabledFormAll(position = 0) {
-
     var elementsInput = this.myForm[position].querySelectorAll('input');
     var elementsSelect = this.myForm[position].querySelectorAll('select');
     for (let i = 0; i < elementsInput.length; i++) {
@@ -78,6 +80,9 @@ class Main {
     }
   }
 
+  /**
+   * The function `disabledFormEdit` disables form elements based on their class name.
+   */
   disabledFormEdit(position = 0) {
     var elementsInput = this.myForm[position].querySelectorAll('input');
     var elementsSelect = this.myForm[position].querySelectorAll('select');
@@ -97,6 +102,9 @@ class Main {
     }
   }
 
+  /**
+   * The `enableFormAll` function enables all input and select elements within a form.
+   */
   enableFormAll(position = 0) {
     var elementsInput = this.myForm[position].querySelectorAll('input');
     var elementsSelect = this.myForm[position].querySelectorAll('select');
@@ -108,6 +116,10 @@ class Main {
     }
   }
 
+  /**
+   * The `resetForm` function clears the input values of all input fields and select elements within a
+   * form and resets the form.
+   */
   resetForm(position = 0) {
     var elementsInput = this.myForm[position].querySelectorAll('input');
     var elementsSelect = this.myForm[position].querySelectorAll('select');
@@ -120,6 +132,14 @@ class Main {
     this.myForm[position].reset();
   }
 
+  /**
+   * The function `getDataFormJson` retrieves form data from input and select elements and returns it
+   * as a JSON object.
+   * @returns The `getDataFormJson()` function returns an object containing the data from the form
+   * elements with IDs in the `myForm` element. The object includes key-value pairs where the key is
+   * the ID of the form element and the value is the trimmed value of the form element (or the checked
+   * status for checkboxes).
+   */
   getDataFormJson(position = 0) {
     var elementsForm = this.myForm[position].querySelectorAll('input, select');
     let getJson = {};
@@ -139,6 +159,12 @@ class Main {
     return getJson;
   }
 
+  /**
+   * The function `getDataFormData` retrieves data from form elements and appends it to a FormData
+   * object.
+   * @returns The `getDataFormData()` function is returning the `fromData` object after populating it
+   * with the data from the form elements.
+   */
   getDataFormData(position = 0) {
     var elementsForm = this.objForm[position].querySelectorAll('input, select');
     elementsForm.forEach(function (element) {
@@ -157,6 +183,14 @@ class Main {
     return this.fromData;
   }
 
+  /**
+   * The function `setDataFormJson` populates form input fields with values from a JSON object based on
+   * element IDs.
+   * @param json - The `json` parameter in the `setDataFormJson` function is expected to be a JavaScript
+   * object that contains key-value pairs where the keys correspond to the `id` attributes of form
+   * elements (input fields or select dropdowns) and the values are the data that should be set in those
+   * form elements
+   */
   setDataFormJson(json, position = 0) {
 
     let elements = this.myForm[position].querySelectorAll("input,select");
@@ -180,6 +214,12 @@ class Main {
     }
   }
 
+  /**
+   * The function `setValidateForm` validates form inputs and selects, displaying error messages and
+   * focusing on the first invalid field if necessary.
+   * @returns The `setValidateForm()` function returns either `true` if the form is valid or `false` if
+   * the form is not valid.
+   */
   setValidateForm(position = 0) {
     const objForm = this.myForm[position];
     const inputs = objForm.querySelectorAll('input');
@@ -204,7 +244,18 @@ class Main {
       return false;
     }
   }
-
+  /**
+   * The function `validateInput` determines the type of input and calls the corresponding validation
+   * function.
+   * @param input - The `input` parameter seems to be an object with a `type` property that determines
+   * the type of validation to be performed. The `validateInput` function then calls different
+   * validation functions based on the `type` of input. The possible types of input for validation are
+   * 'text', 'email',
+   * @returns The `validateInput` function returns the result of calling a specific validation function
+   * based on the input type. If the input type is 'text', it calls `validateText(input)`, if it is
+   * 'email', it calls `validateEmail(input)`, if it is 'password', it calls `validatePassword(input)`,
+   * if it is 'number', it calls `validateNumber(input)`. If
+   */
   validateInput(input) {
     const type = input.type;
     switch (type) {
@@ -221,6 +272,15 @@ class Main {
     }
   }
 
+  /**
+   * The function `validateText` checks if the input value is not empty, not just whitespace, and has a
+   * length of at least 4 characters.
+   * @param input - The `input` parameter in the `validateText` function seems to be an object with a
+   * `value` property. The function checks if the `value` property is an empty string, consists only of
+   * whitespace characters, or has a length less than 4. If any of these conditions are met
+   * @returns The `validateText` function returns `true` if the input value is not empty, not just
+   * whitespace, and has a length of at least 4 characters. Otherwise, it returns `false`.
+   */
   validateText(input) {
     if (input.value === '' || input.value.trim === '' || input.value.length < 4) {
       return false;
@@ -228,25 +288,58 @@ class Main {
     return true;
   }
 
+  /**
+   * The function `validateEmail` uses a regular expression to check if the input value is a valid email
+   * address.
+   * @param input - The `input` parameter in the `validateEmail` function seems to be an object with a
+   * `value` property. The function uses a regular expression to validate whether the value of the
+   * `input` object matches the pattern of a valid email address. If the value passes the validation,
+   * the function returns
+   * @returns The `validateEmail` function is returning a boolean value indicating whether the input
+   * value matches the email regex pattern. If the input value is a valid email address according to the
+   * regex pattern, the function will return `true`, otherwise it will return `false`.
+   */
   validateEmail(input) {
     const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return regex.test(input.value);
   }
-
+  /**
+   * The function `validatePassword` checks if the length of the input password is at least 8
+   * characters and returns true if it meets the criteria, otherwise false.
+   * @param input - The `input` parameter in the `validatePassword` function seems to be an object with
+   * a `value` property. The function checks if the length of the `value` is less than 8 characters and
+   * returns `false` if it is, indicating that the password is not valid. Otherwise,
+   * @returns The function `validatePassword` is returning `true` if the length of the input value is
+   * at least 8 characters, and `false` otherwise.
+   */
   validatePassword(input) {
     if (input.value.length < 8) {
       return false;
     }
     return true;
   }
-
+  /**
+   * The function `validateNumber` checks if the input value is a number in JavaScript.
+   * @param input - The `input` parameter in the `validateNumber` function seems to be an object with a
+   * `value` property. The function checks if the `value` property of the `input` object is a valid
+   * number using the `isNaN` function. If the value is not a number, the function
+   * @returns The function `validateNumber` is returning `true` if the input value is a number, and
+   * `false` if it is not a number.
+   */
   validateNumber(input) {
     if (isNaN(input.value)) {
       return false;
     }
     return true;
   }
-
+  /**
+   * The function `showMessageError` adds an error class to an input element and appends a message
+   * indicating that the field is invalid.
+   * @param input - The `input` parameter in the `showMessageError` function is typically a reference
+   * to an HTML input element that needs to display an error message. The function adds a CSS class
+   * 'error' to the input element to visually indicate an error, and then creates a new `span` element
+   * to display
+   */
   showMessageError(input) {
     input.classList.add('error');
     const messageError = document.createElement('span');
@@ -254,7 +347,10 @@ class Main {
     messageError.textContent = 'This field is invalid';
     input.parentNode.appendChild(messageError);
   }
-
+  /**
+   * The `hiddenMessageError` function clears the inner HTML content of all elements with the class
+   * name 'message-error'.
+   */
   hiddenMessageError() {
     const message = document.querySelectorAll('.message-error');
     for (let data of message) {
@@ -262,6 +358,20 @@ class Main {
     }
   }
 
+/**
+ * The function `createTable` generates an HTML table based on the provided data, ID, and optional
+ * actions.
+ * @param data - The `data` parameter in the `createTable` function is an array of objects that
+ * contains the information to be displayed in the table. Each object in the array represents a row in
+ * the table, and the keys in the object represent the columns.
+ * @param id - The `id` parameter in the `createTable` function is used to specify the id of the HTML
+ * element where the table will be created. This function dynamically generates a table based on the
+ * data provided and populates it within the HTML element identified by the `id` parameter.
+ * @param actions - The `actions` parameter in the `createTable` function is a boolean value that
+ * determines whether to include action buttons in each row of the table. If `actions` is `true`,
+ * action buttons for showing, editing, and deleting the data entry will be added to each row. If
+ * `actions
+ */
   createTable(data, id, actions) {
     const objTable = document.getElementById(id);
     objTable.innerHTML = "";
@@ -269,12 +379,12 @@ class Main {
     let tr = "";
     for (let i = 0; i < Object.keys(data).length; i++) {
       let obj = Object.entries(data[i]);
-      tds = "";
-      for (let j = 0; j < obj.length; j++) {
-        tds += `<td>${obj[j][1]}</td>`;
-      }
+      tds = `<tr><td>${obj[0][1]}</td>`;
+      tds += `<td>${obj[1][1]}</td>`;
+      tds += `<td>${obj[2][1]}</td>`;
+      
       if (actions) {
-        tds += `<td class="text-center"> 
+        tds += `<td class="text-center">
 <div class="btn-group" role="group" aria-label="Basic mixed styles example">
 <button type="button" title="Show" class="btn btn-warning" onclick="${LIST_CRUD[5]}(${obj[0][1]})"><i class="${LIST_CRUD_ICONS[5]}"></i></button>
 <button type="button" title="Edit" class="btn btn-success" onclick="${LIST_CRUD[1]}(${obj[0][1]})"><i class="${LIST_CRUD_ICONS[1]}"></i></button>
@@ -282,45 +392,9 @@ class Main {
 </div>
 </td>`;
       }
-      tr += "<tr>" + tds + "</tr>";
+      tr += tds + "</tr>";
       tds = "";
     }
     objTable.innerHTML = tr;
-    tr = "";
-  }
-
-  createSelect(data, id) {
-    const objSelect = document.getElementById(id);
-    objSelect.innerHTML = "";
-    let options = '<option selected value="">Open this select menu</option>';
-    for (let i = 0; i < Object.keys(data).length; i++) {
-      let obj = Object.entries(data[i]);
-      options += `<option value="${obj[0][1]}">${obj[1][1]}</option>`;
-    }
-    objSelect.innerHTML = options;
-    options = "";
-  }
-
-  createCard(data, id) {
-    const objContainerCard = document.getElementById(id);
-    objContainerCard.innerHTML = "";
-    let card = "";
-
-    for (let i = 0; i < Object.keys(data).length; i++) {
-      let obj = Object.entries(data[i]);
-
-      card += `<div  style="margin: 0.28em !important;" class="card col-3 mx-auto">
-      <img src="https://t3.ftcdn.net/jpg/06/50/85/90/360_F_650859089_x8aWFsiOw5vpE1h936uAMu7UmXmxPcw2.jpg" class="img img-fluid card-img-top " alt="...">
-      <div class="card-body">
-        <h5 class="card-title">${obj[1][1]}</h5>
-        <p class="card-text">${obj[2][1]}.</p>
-        <p class="card-text">${obj[3][1]}</p>
-        <a href="#" onclick="alert(${obj[0][1]})" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>`;
-    
-    }
-    objContainerCard.innerHTML = card;
-    card = "";
   }
 }
